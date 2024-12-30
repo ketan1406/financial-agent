9af54d55a149962b062bda670814a739fc937dfd
Ketan <sainiketan99@gmail.com>
2024-12-30 13:23:57 +0530

    Updated enviroment variable handling

--- financial_agent.py
+++ financial_agent.py
@@ -2,6 +2,13 @@ from phi.agent import Agent
 from phi.model.groq import Groq
 from phi.tools.yfinance import YFinanceTools
 from phi.tools.duckduckgo import DuckDuckGo
+import openai
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 # Web Search Agent Configuration
 
@@ -18,7 +25,6 @@ web_search_agent = Agent(
 # Financial Data Agent Configuration
 finance_agent = Agent(
     name="Financial Data Agent",
-    role="",
     model=Groq(id="llama3-groq-70b-8192-tool-use-preview"),
     tools=[YFinanceTools(stock_price=True, analyst_recommendations=True,
                          stock_fundamentals=True, company_news=True)],
